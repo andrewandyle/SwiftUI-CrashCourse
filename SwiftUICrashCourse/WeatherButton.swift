ffb7feb2f9c7dcd7cda97c4dc2ae243502c2145e
andrewandyle <andrewandyle@gmail.com>
2024-01-28 15:34:46 -0600

    complete crash course

--- SwiftUICrashCourse/WeatherButton.swift
+++ SwiftUICrashCourse/WeatherButton.swift
@@ -5,4 +5,18 @@
 //  Created by Andrew Le on 1/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
