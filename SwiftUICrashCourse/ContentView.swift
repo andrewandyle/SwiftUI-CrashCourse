fc20307cbe3c459d815b2c2379fc277d96391515
andrewandyle <andrewandyle@gmail.com>
2024-01-28 01:46:02 -0600

    Initial Commit

---
+++ SwiftUICrashCourse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICrashCourse
+//
+//  Created by Andrew Le on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICrashCourse/SwiftUICrashCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICrashCourseApp.swift
+//  SwiftUICrashCourse
+//
+//  Created by Andrew Le on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICrashCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
